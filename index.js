9acc96d7081ffe5028acb36a8639c3529dfd11fb
Yoseob Yun <yosub1220@naver.com>
2022-04-03 20:06:23 +0900

    ~10강

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const port = 2000;
+const {User} = require('./models/user.js');
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://yoseobyun:3lxntKUiGWdJ6dwY@boilerplate.o1goy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -9,7 +11,25 @@ mongoose.connect('mongodb+srv://yoseobyun:3lxntKUiGWdJ6dwY@boilerplate.o1goy.mon
 }).then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
+    //어플리케이션에서 urlencoded된 데이터를 분석해서 가져올 수 있도록 설정
+    app.use(bodyParser.urlencoded({extended: true}));
 
+    //json파일을 가질 수 있도록 함
+    app.use(bodyParser.json());
 app.get('/', (req,res) => res.send('Hello World'));
+
+app.post('/register', (req, res) => {
+    //회원가입할 때 필요한 정보를 데이터베이스에 넣어주는 작업, user model 을 이용
+
+    const user = new User(req.body);
+
+    //save를 하기 전에 암호화를 해야한다 -> Mongoose 기능이용
+    user.save((err,userInfo) => { //mongodb에서 오는 메소드로 유저 모델에 저장시키기 위해서 사용한다.
+        if (err) return res.json({success: false, err});  //error가 발생하면, json형식으로 보내주며, err메세지도 같이 보내준다
+        return res.status(200).json({ //성공을 하면 코드 번호 200을 보내주며, 마찬가지로 json파일로 success 를 보낸다
+            success:true
+        })
+    }); 
+});
 
 app.listen(port, () => console.log(`Example app is listening on port ${port}`));
--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -31,6 +33,27 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+userSchema.pre('save', function(next){// 비밀번호 암호화
+    var user = this; // 이 모델 자체를 가리킴
+
+    if(user.isModified('password')){ //이 조건문이 존재하지 않으면
+        bcrypt.genSalt(saltRounds, function(err,salt){
+        if (err) return next(err) //에러가 발생하면 그 전 상황 즉, 비밀번호를 save하던 상황으로 돌아간다
+
+        bcrypt.hash(user.password, salt, function (err, hash){
+            if (err) return next(err) //에러가 발생하면 그 전 상황 즉, 비밀번호를 save하던 상황으로 돌아간다
+            user.password = hash; //기존의 비밀번호를 hash값으로 바꿈
+            next();
+            })
+        })
+    } else {
+        next(); //비밀번호를 바꾸는 경우에만 해당 함수가 실행이 되어야 하기 때문에, 그렇지 않을 경우에는 next를 이용해서 그냥 넘어가게 해야함 
+    }
+
+    
+    
+}); //user모델에 저장하기 전(pre)에 어떠한 조작을 수행하겠다는 의미 
 
 const User = mongoose.model('User', userSchema);
 

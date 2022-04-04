b849fdf18727d90aca11f6f6a760e4c6d5463513
Yoseob Yun <yosub1220@naver.com>
2022-04-04 22:50:04 +0900

    jwt 구현

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
-// 가장 기본이 되는 파일 app.js같은 느낌
+//index.js
 const express = require('express');
 const app = express();
 const port = 2000;
 const {User} = require('./models/user.js');
 const bodyParser = require('body-parser');
-
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://yoseobyun:3lxntKUiGWdJ6dwY@boilerplate.o1goy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected'))
     .catch(err => console.log(err));
 
-    //어플리케이션에서 urlencoded된 데이터를 분석해서 가져올 수 있도록 설정
-    app.use(bodyParser.urlencoded({extended: true}));
-
-    //json파일을 가질 수 있도록 함
-    app.use(bodyParser.json());
+//어플리케이션에서 urlencoded된 데이터를 분석해서 가져올 수 있도록 설정
+app.use(bodyParser.urlencoded({extended: true}));
+//json파일을 body-parser 라이브러리를 통해 해석할 수 있도록 함
+app.use(bodyParser.json());
+app.use(cookieParser());
+//root 페이지
 app.get('/', (req,res) => res.send('Hello World'));
 
-app.post('/register', (req, res) => {
-    //회원가입할 때 필요한 정보를 데이터베이스에 넣어주는 작업, user model 을 이용
-
-    const user = new User(req.body);
-
-    //save를 하기 전에 암호화를 해야한다 -> Mongoose 기능이용
-    user.save((err,userInfo) => { //mongodb에서 오는 메소드로 유저 모델에 저장시키기 위해서 사용한다.
-        if (err) return res.json({success: false, err});  //error가 발생하면, json형식으로 보내주며, err메세지도 같이 보내준다
-        return res.status(200).json({ //성공을 하면 코드 번호 200을 보내주며, 마찬가지로 json파일로 success 를 보낸다
-            success:true
-        })
+//회원가입 페이지
+app.post('/register', (req, res) => { //회원가입할 때 필요한 정보를 데이터베이스에 넣어주는 작업, user model 을 이용
+    const user = new User(req.body); //user라는 변수에 클라이언트가 보낸 body를 담는다. (body에 사용자 정보가 담겨있음)
+    user.save((err,userInfo) => { //mongodb에서 지원해주는 메소드로 유저 모델에 저장시키기 위해서 사용한다.
+        if (err) return res.json({success: false, err})  //error가 발생하면, json형식으로 보내주며, err메세지도 같이 보내준다
+        return res.status(200).json({success:true}) //에러가 발생하지 않고 성공하면 200을 보내준다
     }); 
 });
+
+//로그인 페이지
+app.post('/login' ,(req, res) => {
+    //1. 요청된 이메일이 데이터베이스에 존재하는지 확인
+    User.findOne({email:req.body.email},(err,userInfo) => { //findOne: 해당 정보를 가진 하나의 문서 자체를 찾아내는 메소드
+        if (!userInfo) { //존재하지 않을때 실행
+            return res.json({
+                LoginSuccess: false,
+                message:'제공된 이메일에 해당된 유저가 존재하지 않습니다.'
+            })
+        }
+        //2. 요청된 이메일이 데이터 베이스에 존재한다면 입력한 비밀번호와 데이터베이스에 있는 비밀번호가 일치하는지 확인
+        userInfo.comparePassword(req.body.password,(err,isMatch) => { //이 함수는 user.js에 정의되어 있음
+            if(!isMatch)
+                return res.json({loginSuccess: false, message: "비밀번호가 맞지 않습니다."},err)
+            
+                //3. 다 일치한다면 토큰을 생성
+            userInfo.generateToken((err, userInfo) => {
+                if(err) return res.status(400).send(err);
+                //토큰을 쿠키와 로컬 스토리지에 저장할 수 있는데 강의에서는 쿠키에 저장
+                res.cookie("x_auth",userInfo.token)
+                .status(200)
+                .json({loginSuccess: true, userId: userInfo._id});
+            })
+    })
+});
+});
+
+
 
 app.listen(port, () => console.log(`Example app is listening on port ${port}`));
--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -34,7 +36,7 @@ const userSchema = mongoose.Schema({
     }
 });
 
-userSchema.pre('save', function(next){// 비밀번호 암호화
+userSchema.pre('save', function(next){// index.js에서 입력한 비밀번호를 save하기 전 비밀번호 암호화
     var user = this; // 이 모델 자체를 가리킴
 
     if(user.isModified('password')){ //이 조건문이 존재하지 않으면
@@ -55,6 +57,24 @@ userSchema.pre('save', function(next){// 비밀번호 암호화
     
 }); //user모델에 저장하기 전(pre)에 어떠한 조작을 수행하겠다는 의미 
 
+userSchema.methods.comparePassword = function(plainPassword, callback) { //사용자가 입력한 비밀번호 값(plainpassword | 암호화되어있지 않음)과 기존의 데이터베이스에서의 비밀번호(cipher password|암호화 되어있음)를 비교하는 메소드 제작 -> plainpassword도 암호화해야함
+    bcrypt.compare(plainPassword,this.password, function(err, isMatch){
+        if (err) return callback(err) //에러가 있다면, callback함수에 담아낸후 리턴
+        callback(null,isMatch) //에러가 없다면 에러자리가 null이여야하고 isMatch가 true로 담길것
+    })
+};
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    //jsonwebtoken을 이용해서 token 생성하기
+    var token = jwt.sign(user._id.toHexString(), 'secrettoken'); //jwt.sign: 토큰 발급 -> 데이터베이스에 있는 정보의 id를 토큰화 함 sign 메소드에서는 plainobject를, 즉 text를 기다리므로 기존 값을 string형식으로 바꿔주어야한다.
+    user.token = token;
+    user.save(function(err,user){
+        if (err) return cb(err)
+        cb(null,user);
+    });
+
+};
 const User = mongoose.model('User', userSchema);
 
 module.exports = {User};
